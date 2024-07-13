a99c403acb09219bc25c26fc6cea500be1e960ab
Dilnawaz <mddilnawazalam7739@gmail.com>
2024-07-14 01:17:11 +0530

    Booking Schema added and also added a feature to add trainds by admin

--- backend/Models/Bookings.js
+++ backend/Models/Bookings.js
@@ -1,16 +1,21 @@
 const mongoose = require("mongoose");
 
 const bookingSchema = mongoose.Schema({
-    trainName:{type:String,required:true},
-    trainNumber:{type:String,required:true},
-    source : {type:String,required:true},
-    destination:{type:String,required:true},
-    bookedSeat:{type:Number,required:true},
-    bookingId:{type:String,required:true},
-    bookingTime:{type:String,required:true},
-    userId:{type:String,required:true}
+  name: { type: String, required: true },
+  age: { type: Number, required: true },
+  email: { type: String },
+  mobile: { type: String },
+  address: { type: String },
+  trainName  : { type: String, required: true },
+  trainNumber: { type: String, required: true },
+  source     : { type: String, required: true },
+  destination: { type: String, required: true },
+  bookedSeat : { type: Number, required: true },
+  bookingId  : { type: String, required: true },
+  bookingTime: { type: String, required: true },
+  userId: { type: String, required: true },
 });
 
-const BookingModel = mongoose.model("booking",bookingSchema)
+const BookingModel = mongoose.model("booking", bookingSchema);
 
-module.exports = {bookingSchema,BookingModel}
+module.exports = { bookingSchema, BookingModel };
